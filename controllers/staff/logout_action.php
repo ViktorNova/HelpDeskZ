1fbc295889fa4535fe25057c2932ccf7237c8f4b
evolutionscript <info@evolutionscript.com>
2015-04-09 17:31:11 -0500

    License updated to GPLv2

--- controllers/staff/logout_action.php
+++ controllers/staff/logout_action.php
@@ -1,34 +1,10 @@
 <?php
-/*******************************************************************************
-*  Title: Help Desk Software HelpDeskZ
-*  Version: 1.0 from 17th March 2015
-*  Author: Evolution Script S.A.C.
-*  Website: http://www.helpdeskz.com
-********************************************************************************
-*  COPYRIGHT AND TRADEMARK NOTICE
-*  Copyright 2015 Evolution Script S.A.C.. All Rights Reserved.
-*  HelpDeskZ is a registered trademark of Evolution Script S.A.C..
-
-*  The HelpDeskZ may be used and modified free of charge by anyone
-*  AS LONG AS COPYRIGHT NOTICES AND ALL THE COMMENTS REMAIN INTACT.
-*  By using this code you agree to indemnify Evolution Script S.A.C. from any
-*  liability that might arise from it's use.
-
-*  Selling the code for this program, in part or full, without prior
-*  written consent is expressly forbidden.
-
-*  Using this code, in part or full, to create derivate work,
-*  new scripts or products is expressly forbidden. Obtain permission
-*  before redistributing this software over the Internet or in
-*  any other medium. In all cases copyright and header must remain intact.
-*  This Copyright is in full effect in any country that has International
-*  Trade Agreements with the United States of America
-
-*  Removing any of the copyright notices without purchasing a license
-*  is expressly forbidden. To remove HelpDeskZ copyright notice you must purchase
-*  a license for this script. For more information on how to obtain
-*  a license please visit the page below:
-*  https://www.helpdeskz.com/contact
-*******************************************************************************/
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author Evolution Script S.A.C.
+ * @since 1.0.0
+ */
 staffLogout();
 ?>
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
+/**
+ * @package HelpDeskZ
+ * @website: http://www.helpdeskz.com
+ * @community: http://community.helpdeskz.com
+ * @author Evolution Script S.A.C.
+ * @since 1.0.0
+ */
 include('includes/bootstrap.php');
